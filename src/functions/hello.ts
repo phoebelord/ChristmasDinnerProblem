fb9d7da1c905d16c7c2c178492deb581bca14c96
= <=>
2021-01-29 16:50:25 +0000

    setup eslint and prettier

--- src/functions/hello.ts
+++ src/functions/hello.ts
@@ -1,15 +1,15 @@
-import { APIGatewayProxyHandler } from "aws-lambda";
+import { APIGatewayProxyHandler } from 'aws-lambda';
 
 export const handler: APIGatewayProxyHandler = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Go Serverless v1.0! Your function executed successfully!",
+        message: 'Go Serverless v1.0! Your function executed successfully!',
         input: event,
       },
       null,
-      2
+      2,
     ),
   };
 };
